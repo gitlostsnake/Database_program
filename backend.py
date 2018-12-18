28fa4f18c781946c9db77dc966756702a544deba
gitlostsnake <45536326+gitlostsnake@users.noreply.github.com>
2018-12-18 23:26:24 +0000

    Update backend.py

--- backend.py
+++ backend.py
@@ -26,5 +26,30 @@ def view():
     conn.close()
     return rows
 
+# search function
+def search(location="", client="", startdate="", length=""):
+    conn = sqlite3.connect("road_works.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM road_works WHERE location=? OR client=? OR startdate=? OR length=?",
+                (location, client, startdate, length))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+# delete road works
+def delete(id):
+    conn = sqlite3.connect("road_works.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM road_works WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
 
 connect()
+
+
+# Test functions
+# insert("Location of work", "Client", startdate 18/12/18= 181218, length in # of days= 12)
+# print(search(argument=""))
+# print(view(###this will list all items))
+# delete(1) ### deletes the first item.
