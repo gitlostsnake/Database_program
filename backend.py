3611b5a72a61f3b033f9d17adf015c93772f0d91
gitlostsnake <45536326+gitlostsnake@users.noreply.github.com>
2018-12-18 21:50:27 +0000

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect("road_works.db")
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS road_works(id INTEGER PRIMARY KEY, location text,
+                client text, startdate integer, length interger)""")
+    conn.commit()
+    conn.close()
+
+### add new roadworks
+def insert(location, client, startdate, length):
+    conn = sqlite3.connect("road_works.db")
+    cur = conn.cursor()
+    cur.execute("""INSERT INTO road_works VALUES (NULL, ?, ? ,? ,?),
+                (location, client, startdate, length)""")
+    conn.commit()
+    conn.close()
+
+### view road works
+def view():
+    conn = sqlite3.connect("road_works.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM road_works")
+    rows=cur.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+
+connect()
+
+testing by...
+insert("Newcastle", "martins", 230219, 200)
+print(view())
+
+### getting errors on line 33, 16. sqlite3.OperationalError no such column: location.
