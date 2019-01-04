da031bd403e41d9b1e5ee8471d86ad9dc18064cd
gitlostsnake <45536326+gitlostsnake@users.noreply.github.com>
2019-01-04 01:32:31 +0000

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,17 +1,34 @@
 from tkinter import *
-import backend
+import backend_roadworks
 
-# This is so the backend data can be seen in the display box
+# Click a item in the list
+def get_selected_row(event):
+    global selected_roadworks
+    index = list1.curselection()[0]
+    selected_roadworks = list1.get(index)
+    return(selected_roadworks)
+
+### FUNCTIONS for buttons
 def view_roadworks():
     # Deleteing from index 0 to the end so we dont duplicate items on dispay
     list1.delete(0, END)
-    for row in backend.view():
+    for row in backend_roadworks.view():
+        list1.insert(END, row) # END means every new row is added at the end
+
+### Currently only searches location column
+def search_roadworks():
+    list1.delete(0, END)
+    for row in backend_roadworks.search(search_text.get()):
         list1.insert(END, row)
+
+###
+def delete_roadworks():
+    backend_roadworks.delete(selected_roadworks[0])
 
 
 #### Main
 window=Tk()
-window.title("Stock Management System")
+window.title("TM Organiser")
 
 
 #### Inventory Buttons on the left side
@@ -25,35 +42,47 @@ remove_stk_b = Button(window, text= "Remove Stock", width=12)
 remove_stk_b.grid(row=3, column=0)
 
 
-#### Road works Buttons on the right side
+#### Road works Buttons on the right side NOW ON LEFT
 view_wrks_b = Button(window, text= "View Road Works", width=12,
-                    command=view_roadworks)
-view_wrks_b.grid(row=1, column=6)
+                command = view_roadworks)
+view_wrks_b.grid(row = 4, column = 0)
 
 new_r_wrks_b = Button(window, text= "New Road Works", width=12)
-new_r_wrks_b.grid(row=2, column=6)
+new_r_wrks_b.grid(row = 5, column = 0)
 
-calculate_job = Button(window, text= "Calculate Job", width=12)
-calculate_job.grid(row=3, column=6)
+del_r_wrks_b = Button(window, text= "Del Road Works", width=12,
+                command = delete_roadworks)
+del_r_wrks_b.grid(row= 6, column = 0)
+
+
+### center button under main view box NOW ON LEFT
+calculate_job = Button(window, text = "Calc Job", width=12)
+calculate_job.grid(row = 9, column = 0, columnspan = 1)
+
+
 
 
 
 #### Search bar at the top with an entry box to type into
-search_label = Label(window, text= "Search job #")
-search_label.grid(row=0, column=1)
+# search_label = Label(window, text= "")
+# search_label.grid(row=0, column=1)
 
 search_text = StringVar()
 e1=Entry(window, textvariable= search_text)
-e1.grid(row=0, column=2)
+e1.grid(row=0, column=1)
 
+search_button = Button(window, text= "Search", width = 8,
+                    command = search_roadworks)
+search_button.grid(row=0, column=2)
 
-
-#### Main view box listing items with scroll bar...
-list1 = Listbox(window, height=6,width=35)
-list1.grid(row =1, column =1, rowspan = 6, columnspan = 3)
+### Main view box listing items with scroll bar
+list1 = Listbox(window, height=12,width=70)
+list1.grid(row =1, column =1, rowspan = 15, columnspan = 15)
 
 sb1= Scrollbar(window)
-sb1.grid(row=2 ,column=4, rowspan = 4)
+sb1.grid(row=4 ,column=16, rowspan = 1, columnspan = 2)
+
+list1.bind('<<ListboxSelect>>', get_selected_row)
 
 ### Verticle scroll set to sb1
 list1.configure(yscrollcommand = sb1.set)
